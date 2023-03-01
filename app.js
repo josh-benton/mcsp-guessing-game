f537ffbb5c7784551b7313aea972659d9f74be51
josh-benton <joshuabenton@Joshuas-MacBook-Pro.local>
2023-03-01 18:55:18 -0500

    js basic project through feature 6

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
-let promptGuess = prompt('Which number would you like to guess?');
+const secretNumber = 17;
+const correctMessage = 'Correct';
+const higherMessage = 'Higher';
+const lowerMessage = 'Lower';
+let guessCount = 0;
+let guess;
+let history = [];
+let again;
 
-function guessedNumber(num) {
-    console.log('the guessed number was:' + num);
-    if (num == 17 ) {
-        alert('Correct!');
-    } else if (num > 17) {
-        alert('Lower');
+let userName = prompt('What is your name?');
+
+function checkGuess(guess) {
+    guessCount++;
+    history.push(guess);
+    if (guess == secretNumber) {
+        if (guessCount > 1) {
+            history.pop();
+            alert(`That's ${correctMessage} ${userName}! Your previous guesses were ${history}!`);
+
+        } else {
+            alert(correctMessage);
+        }
+        return true;
+    } else if (guess > secretNumber) {
+        alert(`Sorry ${userName}, guess ${lowerMessage}`)
+        return false;
     } else {
-        alert('Higher');
+        alert(`Sorry ${userName}, guess ${higherMessage}`)
+        return false;
     }
 }
 
 
-console.log(guessedNumber(promptGuess));
+function playAgain() {
+    again = prompt('Would you like to play again? Yes or No.');
+    if (again.toLowerCase() == 'yes') {
+        history = [];
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+do {
+    do {
+        guess = prompt('Which number would you like to guess?');
+    } while (!checkGuess(guess)) {
+    }
+} while (playAgain()) {
+}
+
+
+
